97d21bf2f757e39705cf14f394d3efaaee02f992
kulunu <kulunukesara88@gmail.com>
2025-07-25 01:22:58 +0530

    latest commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000/api';
+const BASE_URL = 'https://backend.ops-company.com/api';
 
 // Axios instance for authenticated requests
 const axiosInstance = axios.create({
